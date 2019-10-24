2dbc6bd196f9857991a50b13734ffb56cb9bb8a9
Gre-z <golang.zgl@gmail.com>
2019-10-24 10:54:34 +0800

    解决common 冲突

--- models.go
+++ models.go
@@ -6,15 +6,8 @@ import (
 )
 
 type Model struct {
-<<<<<<< HEAD
-	Id        int64 `gorm:"primary_key"`
-	CreatedAt jtime.JsonTime
-	UpdatedAt jtime.JsonTime
-	DeletedAt *time.Time `sql:"index"`
-=======
 	Id        int64          `gorm:"primary_key;comment:'主键ID'" json:"id"`
 	CreatedAt jtime.JsonTime `gorm:"comment:'创建时间';type:timestamp not null;default:current_timestamp;" json:"created_at"`
 	UpdatedAt jtime.JsonTime `gorm:"comment:'更新时间';type:timestamp on update current_timestamp;omitempty;default:current_timestamp;" json:"updated_at"`
 	DeletedAt *time.Time     `gorm:"comment:'删除时间'" json:"deleted_at" sql:"index"`
->>>>>>> develop
 }
